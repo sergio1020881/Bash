6f9166b155916a2fabbc85361359c0efa36924d3
sergio <sergio1020881@users.noreply.github.com>
2015-08-31 11:36:32 +0100

    Update diffdelbottom.sh

--- diffdelbottom.sh
+++ diffdelbottom.sh
@@ -11,12 +11,13 @@ declare -rx SCRIPT=${0##*/}
 #Sanity check
 if test -z "$BASH"; #-z
 then
-  printf "$SCRIPT please run this script with the BASH shell"
-  exit 192
+	printf "$SCRIPT please run this script with the BASH shell"
+	exit 192
 fi
 RED=1
 GREEN=2
 QUANT=0
+EXITFLAG=0
 echo "$(tput setaf $GREEN)-------------------------------------------------------------------------- $(tput sgr0)"
 #preset
 IFS_OLD=$IFS
@@ -26,16 +27,16 @@ IFS=$'\0'
 #function exit on interrupt catch
 control_c()
 {
-  TIMESTAMP=$(date +%Y:%m:%d-%H:%M:%S)
-  #troubleshooting
-  rm $LOCATION/.diffdelog.txt -v
-  #
-  echo "$(tput setaf $GREEN)Stopped due to Interrupt «ctr-c» $(tput sgr0)"
-  echo "$TIMESTAMP stopped" >> "$LOGFILE"
-  chmod 444 "$LOGFILE" -v
-  #Cleanup
-  IFS=$IFS_OLD
-  exit #all is well
+ 	TIMESTAMP=$(date +%Y:%m:%d-%H:%M:%S)
+	#troubleshooting
+	rm $LOCATION/.diffdelog.txt -v
+	#
+	echo "$(tput setaf $GREEN)Stopped due to Interrupt «ctr-c» $(tput sgr0)"
+	echo "$TIMESTAMP stopped" >> "$LOGFILE"
+	chmod 444 "$LOGFILE" -v
+	#Cleanup
+	IFS=$IFS_OLD
+	exit #all is well
 }
 #The main
 #GLOBAL VAR
@@ -47,11 +48,11 @@ echo "$(tput setaf $GREEN)------------------------------------------------------
 #filter
 if (( $# < 3 || $# > 3 )); 
 then
-  echo "Usage: $0 LOCATION EXTENSION DEPTH"
-  echo "PARAMETER MISSING !!!"
-  exit
+	echo "Usage: $0 LOCATION EXTENSION DEPTH"
+	echo "PARAMETER MISSING !!!"
+	exit
 else
-  echo "Entry: $0 $1 $2 $3"
+	echo "Entry: $0 $1 $2 $3"
 fi
 #it is always better to make a program that only does the tasks pre-defined, those that are not recognised are to be ignored, this way there is no gaps for errors, only what is expected, otherwise ignore.
 #One way is to compare the input with the one pre-defined or established and only reat to them, others are ignored.
@@ -64,27 +65,27 @@ DIRVAR="$1"
 TERMDIR=$(pwd)
 if [[ $DIRVAR == . ]];
 then
-  LOCATION=$TERMDIR
-  echo "---------------$LOCATION---------------"
+	LOCATION=$TERMDIR
+	echo "---------------$LOCATION---------------"
 else
-  if [[ $DIRVAR == .. ]];
-  then
-    LOCATION=${TERMDIR%/*}
-    echo "---------------$LOCATION---------------"
-  else
-    LOCATION=$DIRVAR
-    echo "---------------$LOCATION---------------"
-  fi
+	if [[ $DIRVAR == .. ]];
+	then
+		LOCATION=${TERMDIR%/*}
+		echo "---------------$LOCATION---------------"
+	else
+		LOCATION=$DIRVAR
+		echo "---------------$LOCATION---------------"
+	fi
 fi
 FOLDER=${LOCATION##*/}
 echo -n "Folder = $LOCATION"
 #
 if [[ ! -d $LOCATION ]];
 then
-  echo " Folder «$LOCATION» does not exist!"
-  exit
+	echo " Folder «$LOCATION» does not exist!"
+	exit
 else
-  echo " Confirmed «$FOLDER»"
+	echo " Confirmed «$FOLDER»"
 fi
 #
 EXTVAR=$2
@@ -98,10 +99,10 @@ echo -n "Depth = $DEPTHVAR"
 #
 if [[ ! $DEPTHVAR =~ ^[0-9]$ ]];
 then
-  echo " Arg 2 must be between 0 and 9!"
-  exit
+	echo " Arg 2 must be between 0 and 9!"
+	exit
 else
-  echo " Confirmed «$DEPTHVAR»"
+	echo " Confirmed «$DEPTHVAR»"
 fi
 #Temporary working file of bash
 TEMPLOGFILE="$LOCATION/.diffdelog.txt"
@@ -112,19 +113,19 @@ LOGFILE="$LOGFOLDER/remove_$extension.txt"
 #testes
 if [[ -d $LOGFOLDER ]];
 then
-  echo -n ""
+	echo -n ""
 else
-  echo "Creating «$LOGFOLDER»"
-  mkdir $LOGFOLDER -v
+	echo "Creating «$LOGFOLDER»"
+	mkdir $LOGFOLDER -v
 fi
 if [[ -f $LOGFILE ]];
 then
-  echo "Updating «$LOGFILE»"
-  chmod 664 $LOGFILE -v
+	echo "Updating «$LOGFILE»"
+	chmod 664 $LOGFILE -v
 else
-  echo "Creating «$LOGFILE»"
-  touch "$LOGFILE"
-  chmod 664 $LOGFILE -v
+	echo "Creating «$LOGFILE»"
+	touch "$LOGFILE"
+	chmod 664 $LOGFILE -v
 fi
 trap control_c SIGINT
 #QUANT=$(ls -R | egrep [!.]\*[.]$extension$ | wc -l)
@@ -143,7 +144,7 @@ j=1
 k=$DEPTHVAR
 for (( j=1; j<=k ; j++ ))
 do
-  find $LOCATION -mindepth $j -maxdepth $j -iname [!.]\*[.]$extension -type f >> $TEMPLOGFILE
+	find $LOCATION -mindepth $j -maxdepth $j -iname [!.]\*[.]$extension -type f >> $TEMPLOGFILE
 done
 QUANT=$(wc -l $TEMPLOGFILE)
 echo "Total Number of Files: $QUANT"
@@ -151,48 +152,56 @@ read -p "Press [Enter] key to start"
 #Interrupt catch (control_c function jump)
 #
 echo "CYCLE START"
-tmp=$(grep -F $extension $TEMPLOGFILE)
-echo "$tmp" | while read -r line;
+tmp="Ignition"
+while [[ -n "$tmp" ]];
 do
-  #line="$(head -1 $TEMPLOGFILE)"
-  tmp=$(grep -vF "$line" $TEMPLOGFILE) #-F
-  echo "$tmp" > $TEMPLOGFILE #faster
-  #
-  printf "Comparator: «%s»\n" $line
-  echo "$(tput setaf $GREEN)-------------------------------------------------------------------------- $(tput sgr0)"
-  echo "Search duplicate of $line:" >> $LOGFILE
-  echo "-------------------------------------------------------------------------- " >> $LOGFILE
-  sleep 5
-  if [[ -n "$line" ]];
-  then
-    echo "$tmp" | while read -r nextline;
-    do
-      if [[ -f "$nextline" ]];
-      then
-        echo -n "-> «$nextline»"
-        charcount=$(diff -N "$line" "$nextline" | wc -c) #-N if not exist treat as empty file
-        echo " Count $charcount"
-        if [[ $charcount -eq 0 ]] && [[ -f "$line" ]] && [[ -f "$nextline" ]]; #safety
-        then
-          #
-          rm "$nextline" -v >> $LOGFILE
-          echo "Removed"
-          tmp=$(grep -vF "$nextline" $TEMPLOGFILE) #-F
-          echo "$tmp" > $TEMPLOGFILE
-        else
-          echo "Stays"
-          continue
-        fi
-      else # $nextline does not exist
-        echo "TARGET EMPTY"
-        tmp=$(grep -vF "$nextline" $TEMPLOGFILE) #-F
-        echo "$tmp" > $TEMPLOGFILE
-        continue
-      fi
-    done
-  else # $line does not exist
-    echo "SOURCE EMPTY"
-  fi
+	tmp=$(grep -F $extension $TEMPLOGFILE)
+	echo "$tmp" | while read -r line;
+	do
+		
+		#line="$(head -1 $TEMPLOGFILE)"
+		tmp=$(grep -vF "$line" $TEMPLOGFILE) #-F
+		echo "$tmp" > $TEMPLOGFILE #faster
+		#
+		printf "Comparator: «%s»\n" $line
+		echo "$(tput setaf $GREEN)-------------------------------------------------------------------------- $(tput sgr0)"
+		echo "Search duplicate of $line:" >> $LOGFILE
+		echo "-------------------------------------------------------------------------- " >> $LOGFILE
+		sleep 5
+		if [[ -n "$line" ]];
+		then
+			echo "$tmp" | while read -r nextline;
+			do
+				
+				if [[ -f "$nextline" ]];
+				then
+					echo -n "-> «$nextline»"
+					charcount=$(diff -N "$line" "$nextline" | wc -c) #-N if not exist treat as empty file
+					echo " Count $charcount"
+					if [[ $charcount -eq 0 ]] && [[ -f "$line" ]] && [[ -f "$nextline" ]]; #safety
+					then
+						#
+						rm "$nextline" -v >> $LOGFILE
+						echo "Removed"
+						tmp=$(grep -vF "$nextline" $TEMPLOGFILE) #-F
+						echo "$tmp" > $TEMPLOGFILE
+						
+					else
+						echo "Stays"
+						continue
+					fi
+				else # $nextline does not exist
+					echo "TARGET EMPTY"
+					tmp=$(grep -vF "$nextline" $TEMPLOGFILE) #-F
+					echo "$tmp" > $TEMPLOGFILE
+				fi
+			done
+			break
+		else # $line does not exist
+			echo "SOURCE EMPTY"
+			continue
+		fi
+	done
 done
 echo "CYCLE END"
 TIMESTAMP=$(date +%Y:%m:%d-%H:%M:%S)
